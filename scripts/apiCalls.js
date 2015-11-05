12ec4912e5a93de8d9f797758939854ec3fed7a2
nilocy <mahaffec@gmail.com>
2015-11-05 17:22:07 -0500

    gets domain and title

--- scripts/apiCalls.js
+++ scripts/apiCalls.js
@@ -1,3 +1,8 @@
 console.log("running");
 // var domain=data.data.children[0].data.domain;
-$.getJSON("http://www.reddit.com/r/music.json",function(data){console.log(data.data.children[0].data);});
+// var title = data.data.children[0].data.title
+$.getJSON("http://www.reddit.com/r/music.json",function(data){
+
+  console.log(data.data.children[0].data.title);
+
+});
