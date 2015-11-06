8dfc3b8b37533ba60df4d365b5ff2d9b7230d5ec
Jacob Shemkovitz <jacob.shemkovitz@gmail.com>
2015-11-06 00:46:49 -0500

    reddit api call

--- scripts/apiCalls.js
+++ scripts/apiCalls.js
@@ -1,8 +1,20 @@
 console.log("running");
-// var domain=data.data.children[0].data.domain;
-// var title = data.data.children[0].data.title
-$.getJSON("http://www.reddit.com/r/music.json",function(data){
 
-  console.log(data.data.children[0].data.title);
+var subreddit = "music";
+var domains = ["youtube.com", "spotify.com", "soundcloud.com", "youtu.be"];
+
+$.getJSON("http://www.reddit.com/r/"+subreddit+".json?limit=100",function(data){
+
+  var results = data.data.children.filter( function( post ){
+    return domains.indexOf( post.data.domain ) !== -1
+  }).map( function( post ) {
+    var result = post.data.title;
+    result = result.split(' [')[0];
+    // result = result.split('{')[0]; // if we want to remove other bracketed items
+    // result = result.split('(')[0];
+    return result;
+  });
+
+  console.log( results );
 
 });
