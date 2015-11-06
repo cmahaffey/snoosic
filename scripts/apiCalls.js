453782605dea50667a34c938249bcbcddf505b35
nilocy <mahaffec@gmail.com>
2015-11-06 10:00:03 -0500

    changes for data

--- scripts/apiCalls.js
+++ scripts/apiCalls.js
@@ -2,7 +2,9 @@ console.log("running");
 // var domain=data.data.children[0].data.domain;
 // var title = data.data.children[0].data.title
 $.getJSON("http://www.reddit.com/r/music.json",function(data){
-
+  for (var i = 0; i < data.data.children.length; i++) {
+    if data.data.children[i].data.domain==="youtube.com"
+  }
   console.log(data.data.children[0].data.title);
 
 });
