9e603902953e0560d2e2891419b168501114c121
Jacob Shemkovitz <jacob.shemkovitz@gmail.com>
2015-11-06 11:04:15 -0500

    add request

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var parser = require('body-parser');
 var path = require('path');
 var mongo = require('mongodb');
+var request = require('request');
 var app = express();
 
 app.use(parser.urlencoded({ extended: false }));
